b5edb93b11f965d1d4a6e386f6546097fe4e324f
Manolo Cantón <manuelcanton8@gmail.com>
2024-08-12 16:10:10 +0200

    parse method can avoid checking the Content-Type

--- addons/http_manager/autoload/response.gd
+++ addons/http_manager/autoload/response.gd
@@ -6,9 +6,13 @@ var successful := false
 var headers := PackedStringArray()
 
 
-func parse() -> Variant:
+func parse(mimetype := MIME.Type.NONE, attributes := {}) -> Variant:
+	if mimetype != MIME.Type.NONE:
+		return MIME.buffer_to_var(body, mimetype, attributes)
+	
 	for h in headers:
 		if h.begins_with("Content-Type:"):
 			var ts := h.substr(14)
 			return MIME.buffer_to_var(body, MIME.string_to_type(ts), MIME.get_attributes(ts))
+	
 	return null
