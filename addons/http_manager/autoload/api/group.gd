50d8c89c949ba7e8bfe0b5cc94ac72b3ca73960d
Manolo Cantón <manuelcanton8@gmail.com>
2024-09-04 22:20:51 +0200

    Improve API group doc

--- addons/http_manager/autoload/api/group.gd
+++ addons/http_manager/autoload/api/group.gd
@@ -1,5 +1,17 @@
 class_name HTTPManagerAPIGroup extends RefCounted
 
+## HTTPManager API Group
+## 
+## Using a routes subfolder, it creates an API route group.
+## [codeblock]
+## # Main api.gd
+## var posts := preload("res://http/myapi/routes/posts/api.gd").new()
+## 
+## # Scene script.
+## var _api := preload("res://http/myapi/api.gd")
+## 
+## _api.posts.search("query").start()
+## [/codeblock]
 ## @experimental
 
 
@@ -12,14 +24,12 @@ func _get_prefix() -> String:
 ## Returns routes dir path.
 func _get_routes_dir() -> String:
 	return ""
+#endregion
 
 ## Returns a new request using a route filename relative to routes dir from
 ## [method _get_base_dir()]. Do not add ".tres".
 ## @deprecated
 func _request(route_filename: String, url_params := {}) -> HTTPManagerRequest:
-	return (load(_get_routes_dir().path_join(route_filename) + ".tres") as HTTPManagerRoute).create_request(url_params)
-#endregion
-
-
-static func create_subgroup(api: HTTPManagerAPI, subpath: String) -> void:
-	api._get_base_dir().path_join(subpath)
+	var route_path := _get_routes_dir().path_join(route_filename) + ".tres"
+	var route := load(route_path) as HTTPManagerRoute
+	return route.create_request(url_params)
